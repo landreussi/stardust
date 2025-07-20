339b4546bc0a1f7ba736bd113c10d7f1923679f7
Lucas Andreussi <lucasandreussi@gmail.com>
2025-07-20 11:02:10 -0300

    feat: repo setting

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Rust development environment with pinned toolchain via rustup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ cargo rustup pkg-config openssl ];
+
+          shellHook = ''
+            rustup component add rust-src rust-analyzer
+          '';
+        };
+      });
+}
